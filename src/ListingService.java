7938e1b5378a8b182421987bf684b768970e026c
MrTACTEC <45337327+MrTACTEC@users.noreply.github.com>
2018-12-20 22:06:34 +0200

    Added searching functionality
    Added the searching functionality and filled the ListingService class' functions.

--- src/ListingService.java
+++ src/ListingService.java
@@ -12,18 +12,31 @@ public class ListingService {
     }
 
     public void createListing(User user, Item item, ArrayList<Question> questions){
-
+        Listing myList = new Listing();
+        myList.setItem(item);
+        myList.setReporter(user);
+        myList.setQuestions(questions);
     }
 
     public void addClaim(Listing listing, Claim claim){
-
+        listing.getClaims().add(claim);
     }
 
     public ArrayList<Claim> retrieveClaims(String listingID){
-
+        return ListingData.getInstance().get(listingID).getClaims();
     }
 
     public void endListing(String listingID){
+        ListingData.getInstance().get(listingID).archive();
+    }
 
+    public Item searchItems(String itemName){
+        ArrayList<Item> Items = ItemData.getInstance().getAll();
+        for (int i=0; i<Items.size()-1; i++){
+            if (itemName == Items.get(i).getName()){
+                return Items.get(i);
+            }
+        }
+        return null;
     }
 }
